2da103e9a076a39c23b4df6376ddf29833bf6514
Diksha Singla <diksha.singla@nagarro.com>
2020-04-21 14:09:37 +0530

    completed both assignments

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Assignment1Component {
 
+  userInput : string;
+
+  onInputProvided(input : string){
+    this.userInput = input;
+  }
 }
--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+
+  @Input()
+  todo:Todo[];
+
+  selectedToDo:Todo;
+
+  hideTable = false;
+
+  onSelectedToDo(todoItem : Todo){
+    this.selectedToDo = todoItem;
+  }
+
+  OnCardHeaderClick(){
+    this.hideTable = !this.hideTable
+  }
 
 }
